cb4463bee1728e154ffb6f261ca394aa5eb7b580
Eduardo Pires da Silva <101749917+EOEDUZADA@users.noreply.github.com>
2024-05-10 15:00:42 -0300

    Add files via upload

---
+++ teste/infoProduto.php
@@ -0,0 +1,197 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/styles.css"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <title>Document</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet">
+</head>
+
+<style>
+
+.container-produtos{
+margin: 0 auto;
+}
+#foto {
+    position: relative;
+    top: 80px;
+    left: 40px;
+width: 150px;
+height: 150px;
+border: 1px solid #000;
+}
+#ncm {
+    position: relative;
+    bottom: 1px;
+}
+
+
+body {
+  font-family: "Inter", sans-serif;
+  font-optical-sizing: auto;
+  font-weight: 400;
+  font-style: normal;
+  font-variation-settings:
+    "slnt" 0;
+}
+
+
+    </style>
+<body>
+
+<div class="container">
+<div class="row">
+    <form class="col s12 m12">
+      <div class="row">
+        <div class="input-field col s1 m1">
+          <input  id="id_produto" type="text" class="validate">
+          <label for="id_produto">ID</label>
+        </div>
+        <div class="input-field col s4 m4">
+          <input id="nome_produto" type="text" class="validate">
+          <label for="nome_produto">Nome do produto</label>
+        </div>
+        <div class="input-field col s4 m4">
+          <input id="ncm" type="text" class="validate">
+          <label for="ncm">NCM</label>
+        </div>
+        <div class="col s3 m3" id="foto">
+      <div> FOTO </div>
+        </div>
+      </div>
+      <div class="row" id="ncm">
+      <div class="input-field col s3">
+          <input  id="unidade" type="text" class="validate">
+          <label for="unidade">Unidade</label>
+        </div>
+        <div class="input-field col s4 offset-s2">
+          <input id="cod_ean" type="text" class="validate">
+          <label for="cod_ean">Cod/EAN</label>
+        </div>
+      </div>
+      <div class="row">
+        <div class="input-field col s1">
+          <input id="peso" type="text" class="validate">
+          <label for="peso">Peso</label>
+        </div>
+        <div class="input-field col s2">
+          <input id="tamanho" type="text" class="validate">
+          <label for="tamanho">Tamanho</label>
+        </div>
+        <div class="input-field col s4 offset-s2">
+          <input id="cod_barra" type="text" class="validate">
+          <label for="cod_barra">Cod de barra</label>
+        </div>
+      </div>
+    </form>
+  </div>
+</div>
+
+
+<div class="container">
+
+<div class="row">
+    <h5> Melhor marca </h5>
+        <div class="input-field col s2">
+          <input id="marca" type="text" class="validate">
+          <label for="marca">Marca</label>
+        </div>
+        <div class="input-field col s2">
+          <input id="modelo" type="text" class="validate">
+          <label for="modelo">Modelo</label>
+        </div>
+        <div class="input-field col s2">
+          <input id="vcr_custo" type="text" class="validate">
+          <label for="vcr_custo">VCR Custo</label>
+        </div>
+        <div class="input-field col s2">
+          <input id="markup" type="text" class="validate">
+          <label for="markup">MARKUP</label>
+        </div>
+        <div class="input-field col s2">
+          <input id="vcr_venda" type="text" class="validate">
+          <label for="vcr_venda">VCR Venda</label>
+        </div>
+      </div>
+</div>
+
+
+<div class="container">
+
+<div class="row">
+    <h5> Estoque </h5>
+        <div class="input-field col s2">
+          <input id="localizacao_estoque" type="text" class="validate">
+          <label for="localizacao_estoque">localizacao_estoque</label>
+        </div>
+        <div class="input-field col s2">
+          <input id="estoque_atual" type="text" class="validate">
+          <label for="estoque_atual">Estoque atual</label>
+        </div>
+        <div class="input-field col s2">
+          <input id="estoque_minimo" type="text" class="validate">
+          <label for="estoque_minimo">Estoque mínimo</label>
+        </div>
+        <div class="input-field col s2">
+          <input id="estoque..." type="text" class="validate">
+          <label for="estoque...">Estoque...</label>
+        </div>
+      </div>
+</div>
+
+
+<div class="container">
+
+<div class="row">
+        <div class="input-field col s1">
+          <input id="marca" type="text" class="validate">
+          <label for="marca">Marca</label>
+        </div>
+        <div class="input-field col s1">
+          <input id="modelo" type="text" class="validate">
+          <label for="modelo">Modelo</label>
+        </div>
+        <div class="input-field col s1">
+          <input id="vcr_compra" type="text" class="validate">
+          <label for="vcr_compra">VCR Compra</label>
+        </div>
+        <div class="input-field col s1">
+          <input id="ipi" type="text" class="validate">
+          <label for="ipi">IPI</label>
+        </div>
+        <div class="input-field col s1">
+          <input id="%_ipi" type="text" class="validate">
+          <label for="%_ipi">%</label>
+        </div>
+        <div class="input-field col s1">
+          <input id="icms_difal" type="text" class="validate">
+          <label for="icms_difal">ICMS-DIFAL</label>
+        </div>
+        <div class="input-field col s1">
+          <input id="%_icms_difal" type="text" class="validate">
+          <label for="%_icms_difal">%</label>
+        </div>
+        <div class="input-field col s1">
+          <input id="frete" type="text" class="validate">
+          <label for="frete">Frete</label>
+        </div>
+        <div class="input-field col s1">
+          <input id="%_frete" type="text" class="validate">
+          <label for="%_frete">%</label>
+        </div>
+        <div class="input-field col s1">
+          <input id="vcr_custo" type="text" class="validate">
+          <label for="vcr_custo">VCR Custo</label>
+        </div>
+       
+      </div>
+</div>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+</body>
+</html>
